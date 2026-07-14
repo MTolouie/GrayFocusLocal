fcd9f49352df3893d451d4c100c1a95b75fe9643
Tankut-Timin <tankutimin@gmail.com>
2026-07-14 14:45:39 +0300

    minor change

--- Wpf/py/test_gui.py
+++ Wpf/py/test_gui.py
@@ -14,6 +14,7 @@ import tkinter as tk
 from tkinter import ttk, filedialog, messagebox
 from PIL import Image, ImageTk
 import numpy as np
+# pyrefly: ignore [missing-import]
 import cv2
 
 # grayscale_clr'ı aynı dizinden yükle
